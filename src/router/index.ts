ec4f5db4f28b40bdca5035e6e4a975bfca55e4b8
Ethan <23275429@qq.com>
2026-04-14 13:20:53 +0800

    切换路由到 Hash 模式，优化 GitHub Pages 部署

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/pages/home.vue'
 import Stats from '@/pages/stats.vue'
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
